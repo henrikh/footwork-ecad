113b3549c522c69f824d9e95d6ea82edaec2ed04
Henrik Enggaard Hansen <henrik.enggaard@gmail.com>
2019-02-26 20:35:11 +0100

    Initial work on testing

---
+++ tests/test_general.py
@@ -0,0 +1,46 @@
+import unittest
+import footwork as fw
+import random
+
+class TestSum(unittest.TestCase):
+
+    def test_two_pad_footprint(self):
+        footprint = fw.Footprint("test")
+
+        footprint.setup_system()
+
+        sys = footprint.system
+        Workplane1 = footprint.workplane
+
+        ### Example of creating and constraining a simple two-pad footprint
+
+        # Pads -- The pads are created in an equation system and a workplane
+        pad1 = fw.Pad(sys, Workplane1, x=-10)
+        pad2 = fw.Pad(sys, Workplane1)
+
+        # The width and height of the pad is constrained
+        fw.Constraint.distance(40, Workplane1, pad1.line_right.a(), pad1.line_right.b())
+        fw.Constraint.distance(50, Workplane1, pad1.line_top.a(), pad1.line_top.b())
+
+        fw.Constraint.equal(Workplane1, pad1.line_right, pad2.line_right)
+        fw.Constraint.equal(Workplane1, pad1.line_top, pad2.line_top)
+
+        # We then fix the distance between the two pads' closest edges
+        desired_distance = random.randint(0, 100)
+        fw.Constraint.distance(desired_distance, Workplane1, pad1.point1, pad2.point2)
+
+        # A construction line is added to fix the pad in space. This construction line
+        # goes between the center of the two pads. The line is fixed to be horizontal
+        # and the origin is at the midpoint.
+        construction_line = fw.LineSegment2d(Workplane1, pad1.point_center, pad2.point_center)
+        fw.Constraint.horizontal(Workplane1, construction_line)
+        fw.Constraint.midpoint(Workplane1, footprint.origin, construction_line)
+
+        footprint.solve()
+
+        distance = abs(pad1.point1.u().value - pad2.point2.u().value)
+
+        self.assertEqual(round(distance, 10), desired_distance)
+
+if __name__ == '__main__':
+    unittest.main()
